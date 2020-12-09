01c708865290ff1c2ea95e76c615851feb1fbc95
vinzcraft38 <lokaleradmin@R510U.training.erfurt.iad.de>
2020-12-09 15:00:27 +0100

    folders  /srv/gitlab/{data,logs,config} in script

--- scripts/gitlab-provision.sh
+++ scripts/gitlab-provision.sh
@@ -2,10 +2,11 @@
 #
 apt-get update
 apt-get -y upgrade
-apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common ## ajout htttps apt + certificat + curl + agent cle gpg +
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - ## ajout package +  ajout gpg signature
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" ## ajout lien repo dans apt
+apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common ## Ajout https apt + certificat + curl + agent cle gpg +
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - ## Ajout package +  ajout gpg signature
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" ## Ajout lien repo dans apt
 apt-get update
-apt-get -y install docker-ce docker-ce-cli containerd.io ## install docker comuniti edition + docker ce cli + containerd
-usermod -aG docker vagrant ##ajout user vagrant in docker group
-systemctl enable docker ##auto startup docker
+apt-get -y install docker-ce docker-ce-cli containerd.io ## Install docker comuniti edition + docker ce cli + containerd
+usermod -aG docker vagrant ## Ajout user vagrant in docker group
+systemctl enable docker ## Ajout auto startup docker
+mkdir -p /srv/gitlab/{data,logs,config} ## Ajout dossier serveur gitlab
