7aacc48af1626df005de672a4628263952362a32
vinzcraft38 <lokaleradmin@R510U.training.erfurt.iad.de>
2020-12-09 12:43:54 +0100

    Provision: Docker Installiert

--- scripts/gitlab-provision.sh
+++ scripts/gitlab-provision.sh
@@ -2,3 +2,10 @@
 # Hier kommt Bockwurst rein
 apt-get update
 apt-get -y upgrade
+apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io
+usermod -aG docker vagrant ##ajout user vagrant in docker group
+systemctl enable docker ##auto startup docker
